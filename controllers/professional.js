f52af88000b723f95b85c0d9ed509a3f1971754b
tomaseliasfontes <tomaseliasfontes@gmail.com>
2025-03-10 18:14:03 -0300

    Fix some issues

--- controllers/professional.js
+++ controllers/professional.js
@@ -1,11 +1,14 @@
 const mongodb = require('../db/connect');
 
-const getData = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('user').find();
-  result.toArray().then((lists) => {
+const getData = async (req, res) => {
+  try {
+    const db = mongodb.getDb();
+    const result = await db.collection('contacts').find().toArray();
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]); // we just need the first one (the only one)
-  });
+    res.status(200).json(result); // Ahora devolvemos toda la lista de contactos
+  } catch (err) {
+    res.status(500).json({ error: 'Error retrieving contacts', details: err });
+  }
 };
 
 module.exports = { getData };
--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
 const professionalRoutes = require('./routes/professional');
 
-const port = process.env.PORT || 8080;
 const app = express();
+const port = process.env.PORT || 8080;
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-  .use('/professional', professionalRoutes);
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
 
-mongodb.initDb((err, mongodb) => {
+app.use('/professional', professionalRoutes);
+
+mongodb.initDb((err) => {
   if (err) {
     console.log(err);
   } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
+    app.listen(port, '0.0.0.0', () => console.log(`Connected to DB and listening on port ${port}`));
   }
 });
