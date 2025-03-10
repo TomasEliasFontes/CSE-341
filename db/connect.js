8188ca01b22b40e8db6cbf0a34c444079b7f0a17
tomaseliasfontes <tomaseliasfontes@gmail.com>
2025-03-10 18:15:37 -0300

    Change database

--- db/connect.js
+++ db/connect.js
@@ -1,22 +1,23 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 
 let _db;
 
-const initDb = (callback) => {
+const initDb = async (callback) => {
   if (_db) {
     console.log('Db is already initialized!');
     return callback(null, _db);
   }
-  MongoClient.connect(process.env.MONGODB_URI)
-    .then((client) => {
-      _db = client;
-      callback(null, _db);
-    })
-    .catch((err) => {
-      callback(err);
-    });
+  
+  try {
+    const client = await MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    _db = client.db(); // Selecciona la base de datos
+    console.log('Database connected!');
+    return callback(null, _db);
+  } catch (err) {
+    return callback(err);
+  }
 };
 
 const getDb = () => {
@@ -26,7 +27,4 @@ const getDb = () => {
   return _db;
 };
 
-module.exports = {
-  initDb,
-  getDb,
-};
+module.exports = { initDb, getDb };
