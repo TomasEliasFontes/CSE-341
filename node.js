ebae63565b09a04ef3e9324f1f1dda2cb8f4ebcd
tomaseliasfontes <tomaseliasfontes@gmail.com>
2025-03-10 17:35:04 -0300

    Try week 1 activity

--- node.js
+++
@@ -1,9 +0,0 @@
-const {MongoClient} = require('mongodb');
-
-async function main() {
-	// we'll add code here soon
-}
-
-const dotenv = require("dotenv")
-
-dotenv.config()
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-const express = requiere("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./db/connect');
+const professionalRoutes = require('./routes/professional');
+
+const port = process.env.PORT || 8080;
 const app = express();
 
-const Port = process.env.Port || 3000;
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/professional', professionalRoutes);
 
-app.listen(Port, () => console.log("Server started"))
-
-
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
